c92c8cfc8ef710a8ee4697a65d8bd4a69dae71f4
Jacob Smits <jacobsmits2509@gmail.com>
2016-07-25 11:44:48 -0500

    Fixed linkedin not opening in new tab

--- resources/views/jst/home.blade.php
+++ resources/views/jst/home.blade.php
@@ -6,7 +6,7 @@
 				<div class="social">
 					<a class="social_link facebook" href="https://www.facebook.com/jacob.smits1" target="_blank"><span class="socicon-facebook"></span></a>
 					<a class="social_link google" href="https://www.google.com/+JacobSmits" target="_blank"><span class="socicon-google"></span></a>
-					<a class="social_link linkedin" href="https://www.linkedin.com/in/jacob-smits-1195033b"><span class="socicon-linkedin" target="_blank"></span></a>
+					<a class="social_link linkedin" href="https://www.linkedin.com/in/jacob-smits-1195033b" target="_blank"><span class="socicon-linkedin" target="_blank"></span></a>
 					<a class="social_link stackoverflow" href="http://stackexchange.com/users/400864/jacob" target="_blank"><span class="socicon-stackoverflow"></span></a>
 					<a class="social_link github" href="https://github.com/throttlehead" target="_blank"><span class="socicon-github"></span></a>
 				</div>
