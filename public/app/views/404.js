aceb80fdd90e8dcd2350d8e49b03d1c760f08dbd
Jacob Smits <jacobsmits2509@gmail.com>
2016-07-25 17:28:55 -0500

    Created 404

--- public/app/views/404.js
+++ public/app/views/404.js
@@ -7,10 +7,20 @@ define([
 ], function($, _, Backbone, BaseView){
 
   var NotFound = BaseView.extend({
-    className: 'home',
+    id: '404',
+    className: '404 view dark',
+
 
     initialize: function(options) {
       BaseView.prototype.initialize.apply(this, arguments);
+    },
+
+
+    render: function() {
+      var template = _.template( window.templates.find("#404_t").html());
+      this.$el.html( template );
+      this.hideLoader();
+      return this;      
     }
 
   });
