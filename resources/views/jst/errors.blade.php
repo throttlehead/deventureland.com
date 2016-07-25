47cfdc24db0273e1bbe96bc0c57c1689ce4ecccb
Jacob Smits <jacobsmits2509@gmail.com>
2016-07-25 17:39:44 -0500

    Simplified page not found view

--- resources/views/jst/errors.blade.php
+++ resources/views/jst/errors.blade.php
@@ -1,9 +1,10 @@
 <script type="text/template" id="404_t">
 	<div class="row">
 		<div class="col-lg-6 col-md-10 col-sm-12 col-lg-offset-3 col-md-offset-1">
-			<div class="card card-brand" style="margin-top: 300px">
+			<div class="card card-brand" style="margin-top: 25%">
 				<div class="card-block">
-					<h1 class="text-center"><span class="glyphicons glyphicons-exclamation-sign"></span> Page not found</h1>
+          <h1 class="text-center text-brand"><span class="glyphicons glyphicons-exclamation-sign"></span></h1>
+          <h3 class="text-center">Page not found</h3>
 				</div>
 			</div>
 		</div>
