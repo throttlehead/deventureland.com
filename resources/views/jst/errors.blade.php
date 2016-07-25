c14ed9fdc97d6f360eb161abb2fe3489718b0125
Jacob Smits <jacobsmits2509@gmail.com>
2016-07-25 17:48:13 -0500

    Improved error card styling

--- resources/views/jst/errors.blade.php
+++ resources/views/jst/errors.blade.php
@@ -1,12 +1,8 @@
 <script type="text/template" id="404_t">
-	<div class="row">
-		<div class="col-lg-6 col-md-10 col-sm-12 col-lg-offset-3 col-md-offset-1">
-			<div class="card card-brand" style="margin-top: 25%">
-				<div class="card-block">
-          <h1 class="text-center text-brand"><span class="glyphicons glyphicons-exclamation-sign"></span></h1>
-          <h3 class="text-center">Page not found</h3>
-				</div>
-			</div>
+	<div class="card card-brand error-card">
+		<div class="card-block">
+      <h1 class="text-center text-brand"><span class="glyphicons glyphicons-exclamation-sign"></span></h1>
+      <h3 class="text-center">Page not found</h3>
 		</div>
 	</div>
 </script>
